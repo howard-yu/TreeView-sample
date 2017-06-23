797702fff3629ea825a26c5d41b4591c3c039a82
howard.yu <howard.yu@dfi.com>
2017-06-23 17:16:13 +0800

    [ADD]treeview sample

---
+++ TreeView/index.js
@@ -0,0 +1,196 @@
+﻿function initjsTree() {
+    var jscom = jsTreeCom.getInstance();
+
+    $('#html').jstree();
+
+    $('#treeview').jstree({
+        'core': {
+            'data': [{
+                "id": "1",
+                "text": "Release",
+                "children": [
+                    {
+                        "id": "2", "text": "Braswell",
+                        "children": [
+                            {
+                                "id": "1_2_21", "text": "BW051",
+                                "children": [
+                                   { "id": "1_2_21_211", "text": "Win10" },
+                                   { "id": "1_2_21_212", "text": "Win8 X" },
+                                   { "id": "1_2_21_213", "text": "Win7" },
+                                   {
+                                       "id": "1_2_21_214", "text": "WES7",
+                                       "children": [
+                                           { "id": "1_2_21_214_2141", "text": "64bit" },
+                                           { "id": "1_2_21_214_2142", "text": "32bit" }
+                                       ]
+                                   },
+                                ]
+                            },
+                            { "id": "1_2_22", "text": "BW173" },
+                            { "id": "1_2_23", "text": "BW556" }
+                        ],
+                    },
+                    {
+                        "id": "3", "text": "Skylake",
+                        "children": [
+                            { "id": "1_3_21", "text": "test3" },
+                            { "id": "1_3_22", "text": "test4" }
+                        ],
+                    }
+                ]
+            }],
+            "themes": {
+                "dots": true,
+                "responsive": false
+            },
+            'multiple': false,
+            'check_callback': function (operation, node, parent, position, more) {
+                var options = {
+                    'operation': operation,
+                    'node': node,
+                    'parent': parent,
+                    'position': position,
+                    'more': more
+                };
+                jscom.setOptions(options);
+                jscom.checkCallback();
+            },
+        },
+        'types': {
+            "default": {
+                "icon": "jstree-default-large jstree-folder"
+            },
+            "file": {
+                "icon": "jstree-default-large jstree-file",
+                "valid_children" : []
+            },
+        },
+        'plugins': [
+            'dnd',
+            'state',
+            'types',
+            'contextmenu',
+            'wholerow',
+        ],
+        "contextmenu": {
+            select_node: false,
+            show_at_node: true,
+            items: function (options, callback) {
+                var actionitem = jscom.createActions();
+                var actions = {
+                    "create": options.type == "file" ? null : actionitem.create,
+                    "rename": (options.id == "1" || options.type == "file") ? null: actionitem.rename,
+                    "delete": (options.id == "1" || options.type == "file") ? null : actionitem.delete
+                }
+                return actions;
+            }
+        },
+    });
+
+    $('#treeview').on("select_node.jstree", function (e, data) {
+
+        var loMainSelected = data;
+        var nodepath = uiGetParents(loMainSelected);
+
+        $('#wrapper-content').empty();
+        var content = "<table class='table'>";
+        content += "<col width='80%'>";
+        content += "<tr>";
+        content += "<td colspan='8'>";
+        content += "<div class='row'>";
+        content += "<div class='col-md-4'>" + nodepath + "</div>";
+        content += "<div class='col-md-8' style='text-align:right'>";
+        content += "<input type='button' value='Add New File' id='add' />";
+        content += "</div>";
+        content += "</div>";
+        content += "</td>";
+        content += "</tr>";
+        content += "<tr>";
+        content += "<th colspan='8'><div class='text-center'>Standard</div></th>";
+        content += "</tr>";
+        content += "<tr>";
+        content += "<td colspan='8'><div class='text-center'>OS</div></td>";
+        content += "</tr>";
+        content += "<tr>";
+        content += "<td class='text-center'>File Name</td>";
+        content += "<td class='text-center'>Part Number</td>";
+        content += "<td class='text-center'>Revision</td>";
+        content += "<td class='text-center'>Release Date</td>";
+        content += "<td class='text-center'>Upload Date</td>";
+        content += "<td class='text-center'>Desc</td>";
+        content += "<td class='text-center'>Status</td>";
+        content += "<td class='text-center'>";
+        content += "</td>";
+        content += "</tr>";
+        content += "<tr>";
+        content += "<td class='text-center'><a href='#'>OS_WES7_642_0000BWE7</a><span class='glyphicon glyphicon-save'></span></td>";
+        content += "<td class='text-center'>642_0000BWE7_080G</td>";
+        content += "<td class='text-center'>1.0.0.0</td>";
+        content += "<td class='text-center'>2017/5/4</td>";
+        content += "<td class='text-center'>2017/5/4</td>";
+        content += "<td class='text-center'>WES7 OS Img</td>";
+        content += "<td class='text-center'>Release for PA</td>";
+        content += "<td class='text-center'>";
+        content += "<input type='button' value='Formal Release' id='formalrelease'/><br />";
+        content += "<input type='button' value='Edit' id='edit' />";
+        content += "<input type='button' value='Delete' id='delete' />";
+        content += "</td>";
+        content += "</tr>";
+        content += "<tr>";
+        content += "<th colspan='8'><div class='text-center'>OEM</div></th>";
+        content += "</tr>";
+        content += "<tr>";
+        content += "<td colspan='8'><div class='text-center'>Driver</div></td>";
+        content += "</tr>";
+        content += "<tr>";
+        content += "<td colspan='8'><div class='text-center'>Audio</div></td>";
+        content += "</tr>";
+        content += "<tr>";
+        content += "<td class='text-center'>File Name</td>";
+        content += "<td class='text-center'>Part Number</td>";
+        content += "<td class='text-center'>Revision</td>";
+        content += "<td class='text-center'>Release Date</td>";
+        content += "<td class='text-center'>Upload Date</td>";
+        content += "<td class='text-center'>Desc</td>";
+        content += "<td class='text-center'>Status</td>";
+        content += "<td class='text-center'>";
+        content += "</td>";
+        content += "</tr>";
+        content += "<tr>";
+        content += "<td class='text-center'><a href='#'>OS_WES7_642_0000BWE7</a><span class='glyphicon glyphicon-save'></span></td>";
+        content += "<td class='text-center'>642_0000BWE7_080G</td>";
+        content += "<td class='text-center'>1.0.0.0</td>";
+        content += "<td class='text-center'>2017/5/4</td>";
+        content += "<td class='text-center'>2017/5/4</td>";
+        content += "<td class='text-center'>WES7 OS Img</td>";
+        content += "<td class='text-center'>Release for PA</td>";
+        content += "<td class='text-center'>";
+        content += "<input type='button' value='Edit' id='edit' />";
+        content += "<input type='button' value='Delete' id='delete' />";
+        content += "</td>";
+        $('#wrapper-content').append(content);
+    });
+};
+
+function uiGetParents(loSelectedNode) {
+    try {
+        var lnLevel = loSelectedNode.node.parents.length;
+        var lsSelectedID = loSelectedNode.node.id;
+        var loParent = $("#" + lsSelectedID);
+        var lsParents = loSelectedNode.node.text + ' >';
+        for (var ln = 0; ln <= lnLevel - 1 ; ln++) {
+            var loParent = loParent.parent().parent();
+            if (loParent.children()[2] != undefined) {
+                lsParents += loParent.children()[2].text + " > ";
+            }
+        }
+        if (lsParents.length > 0) {
+            lsParents = lsParents.substring(0, lsParents.length - 1);
+        }
+        return lsParents;
+    }
+    catch (err) {
+        alert('Error in uiGetParents');
+    }
+};
---
+++ TreeView/jstree-component.js
@@ -0,0 +1,121 @@
+﻿var jsTreeCom = (function () {
+    var instance;
+
+    function createInstance() {
+        var object = new jstreeCom();
+        return object;
+    };
+
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
+
+function jstreeCom() {
+    this.options = {};
+};
+
+jstreeCom.prototype.setOptions = function (options) {
+    this.options = options;
+};
+
+jstreeCom.prototype.checkCallback = function () {
+    if (this.options.operation === 'create_node') {
+        console.log(this.options.parent.text);
+        console.log('create');
+    }
+    if (this.options.operation === 'delete_node') {
+        console.log(this.options.node.text);
+        console.log(this.options.position);
+        console.log('delete');
+    }
+    if (this.options.operation === 'rename_node') {
+        console.log(this.options.position);
+        console.log(this.options.node.text);
+        console.log('rename');
+    }
+    return true;
+};
+
+jstreeCom.prototype.createActions = function ()
+{
+    var actions = {};
+    actions.create = {
+        "separator_before": false,
+        "separator_after": true,
+        "_disabled": false,
+        "label": "new",
+        "action": function (data) {
+            var inst = $.jstree.reference(data.reference),
+                 obj = inst.get_node(data.reference);
+            inst.create_node(obj, {}, "first", function (new_node) {
+                setTimeout(function () { inst.edit(new_node); }, 0);
+            });
+        }
+    }
+    actions.rename = {
+        "separator_before": false,
+        "separator_after": false,
+        "_disabled": false, //(this.check("rename_node", data.reference, this.get_parent(data.reference), "")),
+        "label": "rename",
+        "action": function (data) {
+            var inst = $.jstree.reference(data.reference),
+                    obj = inst.get_node(data.reference);
+            inst.edit(obj);
+        }
+    }
+    actions.delete = {
+        "separator_before": false,
+        "icon": false,
+        "separator_after": false,
+        "_disabled": false, //(this.check("delete_node", data.reference, this.get_parent(data.reference), "")),
+        "label": "delete",
+        "action": function (data) {
+            var inst = $.jstree.reference(data.reference),
+                    obj = inst.get_node(data.reference);
+            if (inst.is_selected(obj)) {
+                inst.delete_node(inst.get_selected());
+            }
+            else {
+                inst.delete_node(obj);
+            }
+        }
+    }
+    return actions ;
+}
+
+jstreeCom.prototype.post = function (url, postData, successFunction, errorFunction) {
+    var result;
+    $.support.cors = true;
+    $.ajax({
+        type: "POST",
+        url: url,
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify(postData),
+        crossDomain: true,
+        success: function (data, textStatus, jqXHR) {
+            successFunction(data, errorFunction);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            try {
+                if (jqXHR.responseText) {
+                    result = JSON.parse(jqXHR.responseText);
+                    result = result.message;
+                } else {
+                    result = errorThrown;
+                }
+            }
+            catch (e) {
+                result = errorThrown;
+            }
+
+            errorFunction(result);
+        }
+    });
+}
